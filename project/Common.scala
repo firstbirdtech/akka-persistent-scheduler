df3f2c6c5085678298e711f26e18b51d7c0f4662
pedias <pedrozte@gmail.com>
2017-04-20 15:51:10 +0200

    add travis and bintray and dynver plugins

---
+++ project/Common.scala
@@ -0,0 +1,38 @@
+import sbt.Keys._
+import sbt._
+
+object Common extends AutoPlugin {
+  override def trigger: PluginTrigger = allRequirements
+  override def requires: Plugins      = plugins.JvmPlugin
+
+  override lazy val projectSettings = Seq(
+    organization := "com.firstbird",
+    organizationName := "Firstbird GmbH",
+    homepage := Some(url("https://github.com/firstbirdtech/akka-persistent-scheduler")),
+    scmInfo := Some(ScmInfo(url("https://github.com/firstbirdtech/akka-persistent-scheduler"), "git@github.com:firstbirdtech/akka-persistent-scheduler.git")),
+    developers += Developer("contributors", "Contributors", "hello@firstbird,com", url("https://github.com/firstbirdtech/akka-persistent-scheduler/graphs/contributors")),
+    licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
+
+    scalaVersion := "2.11.8",
+    scalacOptions ++= Seq(
+      "-encoding",
+      "UTF-8",
+      "-feature",
+      "-unchecked",
+      "-deprecation",
+      "-Xfatal-warnings",
+      "-Xlint",
+      "-Yno-adapted-args",
+      "-Ywarn-dead-code",
+      "-Xfuture"
+    ),
+    javacOptions ++= Seq(
+      "-Xlint:unchecked"
+    ),
+    // show full stack traces and test case durations
+    testOptions in Test += Tests.Argument("-oDF"),
+    resolvers ++= Seq(
+      Resolver.mavenLocal
+    )
+  )
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,8 @@
-// The Play plugin
-resolvers += "Sonatype Snaopshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+addSbtPlugin("com.geirsson"  % "sbt-scalafmt"        % "0.5.6")
+addSbtPlugin("org.scoverage" % "sbt-scoverage"       % "1.5.0")
+addSbtPlugin("com.codacy"    % "sbt-codacy-coverage" % "1.3.8")
+addSbtPlugin("com.dwijnand"  % "sbt-dynver"          % "1.2.0")
+addSbtPlugin("me.lessis"     % "bintray-sbt"         % "0.3.0-8-g6d0c3f8")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin"          % "2.5.13")
-addSbtPlugin("com.eed3si9n"      % "sbt-assembly"        % "0.14.4")
-addSbtPlugin("com.github.gseitz" % "sbt-release"         % "1.0.4")
-addSbtPlugin("com.geirsson"      % "sbt-scalafmt"        % "0.6.2")
-addSbtPlugin("com.tapad"         % "sbt-docker-compose"  % "1.0.19")
-addSbtPlugin("org.scoverage"     % "sbt-scoverage"       % "1.5.0")
-addSbtPlugin("com.codacy"        % "sbt-codacy-coverage" % "1.3.8")
+resolvers += Resolver.url("2m-sbt-plugin-releases", url("https://dl.bintray.com/2m/sbt-plugin-releases/"))(
+  Resolver.ivyStylePatterns)
