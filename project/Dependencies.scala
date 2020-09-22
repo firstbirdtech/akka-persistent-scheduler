a5f0aa48928922d81bbc13456c54fdb409d9067b
Fabian Grutsch <f.grutsch@gmail.com>
2020-09-22 12:14:10 +0200

    fix compliation for scala 2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,11 +5,12 @@ object Dependencies {
   private val akkaVersion = "2.6.9"
 
   val core = Seq(
-    "org.scala-lang.modules" %% "scala-java8-compat"  % "0.9.1",
-    "com.typesafe.akka"      %% "akka-actor"          % akkaVersion,
-    "com.typesafe.akka"      %% "akka-testkit"        % akkaVersion % Test,
-    "com.miguno.akka"        %% "akka-mock-scheduler" % "0.5.5"     % Test,
-    "org.scalatest"          %% "scalatest"           % "3.2.2"     % Test
+    "org.scala-lang.modules" %% "scala-collection-compat" % "2.2.0",
+    "org.scala-lang.modules" %% "scala-java8-compat"      % "0.9.1",
+    "com.typesafe.akka"      %% "akka-actor"              % akkaVersion,
+    "com.typesafe.akka"      %% "akka-testkit"            % akkaVersion % Test,
+    "com.miguno.akka"        %% "akka-mock-scheduler"     % "0.5.5"     % Test,
+    "org.scalatest"          %% "scalatest"               % "3.2.2"     % Test
   )
 
 }
