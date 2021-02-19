bb54bed1cab6aae4b8a73a5438f4b18f1d84ef08
Fabian Grutsch <f.grutsch@gmail.com>
2021-02-19 10:08:01 +0100

    introduce github actions/remove travisci and align project and plugins (#28)

--- core/src/main/scala/persistentscheduler/impl/PersistentScheduler.scala
+++ core/src/main/scala/persistentscheduler/impl/PersistentScheduler.scala
@@ -1,6 +1,25 @@
-package persistentscheduler.impl
+/*
+ * Copyright (c) 2021 Akka Persistent Scheduler contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-import java.time.Instant
+package persistentscheduler.impl
 
 import akka.actor.{Actor, ActorRef, Cancellable, Props}
 import akka.pattern.pipe
@@ -8,6 +27,7 @@ import persistentscheduler.impl.PersistentScheduler._
 import persistentscheduler.scaladsl.SchedulerPersistence
 import persistentscheduler.{SchedulerSettings, TimedEvent, _}
 
+import java.time.Instant
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
@@ -47,7 +67,7 @@ private[impl] class PersistentScheduler(persistence: SchedulerPersistence, setti
     extends Actor
     with AkkaScheduler {
 
-  private implicit val ec: ExecutionContext = context.dispatcher
+  implicit private val ec: ExecutionContext = context.dispatcher
 
   private var subscriptions: Set[Subscription]     = Set.empty
   private var nextEvent: Option[TimedEvent]        = None
