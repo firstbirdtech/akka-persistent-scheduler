b5b2eb2b4b744ea6c4122e6d65a26cd63c388fec
Scala Steward <me@scala-steward.org>
2021-02-23 20:32:00 +0100

    Update akka-actor, akka-testkit to 2.6.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val akkaVersion = "2.6.9"
+  private val akkaVersion = "2.6.13"
 
   val core = Seq(
     "org.scala-lang.modules" %% "scala-collection-compat" % "2.4.2",
